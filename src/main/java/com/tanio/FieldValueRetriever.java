9b8785950382555f12bfa3fae729d09568257174
taniomessina <taniomessina@gmail.com>
2023-03-05 19:50:51 +0000

    Extract FieldValueRetriever

---
+++ src/main/java/com/tanio/FieldValueRetriever.java
@@ -0,0 +1,44 @@
+package com.tanio;
+
+import java.lang.reflect.Method;
+
+class FieldValueRetriever {
+    Object retrieveFieldValue(String fieldName, Object entity) {
+        final String separator = ".";
+        int dotIndex = fieldName.indexOf(separator);
+
+        if (dotIndex == -1) {
+            String methodName = getterMethodName(fieldName);
+            return invokeMethod(methodName, entity);
+        }
+
+        String topLevelFieldName = extractTopLevelFieldName(fieldName, dotIndex);
+        String methodName = getterMethodName(topLevelFieldName);
+        Object topLevelFieldValue = invokeMethod(methodName, entity);
+        String remainingLevelFieldNames = extractRemainingLevelFieldNames(fieldName, dotIndex);
+        return retrieveFieldValue(remainingLevelFieldNames, topLevelFieldValue);
+    }
+
+    private static String extractRemainingLevelFieldNames(String fieldName, int dotIndex) {
+        return fieldName.substring(dotIndex + 1);
+    }
+
+    private static String extractTopLevelFieldName(String fieldName, int dotIndex) {
+        return fieldName.substring(0, dotIndex);
+    }
+
+    private static Object invokeMethod(String methodName, Object entity) {
+        Object methodInvocationResult;
+        try {
+            Method method = entity.getClass().getMethod(methodName);
+            methodInvocationResult = method.invoke(entity);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return methodInvocationResult;
+    }
+
+    private static String getterMethodName(String fieldName) {
+        return "get" + fieldName.substring(0, 1).toUpperCase() + fieldName.substring(1);
+    }
+}
--- src/main/java/com/tanio/Filter.java
+++ src/main/java/com/tanio/Filter.java
@@ -1,11 +1,11 @@
 package com.tanio;
 
-import java.lang.reflect.Method;
 import java.util.List;
 import java.util.stream.Collectors;
 
 class Filter {
-    FieldConditionEvaluator fieldConditionEvaluator = new FieldConditionEvaluator();
+    private final FieldConditionEvaluator fieldConditionEvaluator = new FieldConditionEvaluator();
+    private final FieldValueRetriever fieldValueRetriever = new FieldValueRetriever();
 
     List<Entity> perform(List<Entity> target, Condition condition) {
         return target.stream()
@@ -14,49 +14,10 @@ class Filter {
     }
 
     private boolean matchesCondition(Condition condition, Entity entity) {
-        Object fieldValue = getFieldValue(condition.fieldName, entity);
+        Object fieldValue = fieldValueRetriever.retrieveFieldValue(condition.fieldName, entity);
         return fieldConditionEvaluator.evaluateCondition(
                 condition.operator,
                 condition.value,
                 fieldValue);
-    }
-
-    private static Object getFieldValue(String fieldName, Object entity) {
-        final String separator = ".";
-        int dotIndex = fieldName.indexOf(separator);
-
-        if (dotIndex == -1) {
-            String methodName = getterMethodName(fieldName);
-            return invokeMethod(methodName, entity);
-        }
-
-        String topLevelFieldName = extractTopLevelFieldName(fieldName, dotIndex);
-        String methodName = getterMethodName(topLevelFieldName);
-        Object topLevelFieldValue = invokeMethod(methodName, entity);
-        String remainingLevelFieldNames = extractRemainingLevelFieldNames(fieldName, dotIndex);
-        return getFieldValue(remainingLevelFieldNames, topLevelFieldValue);
-    }
-
-    private static String extractRemainingLevelFieldNames(String fieldName, int dotIndex) {
-        return fieldName.substring(dotIndex + 1);
-    }
-
-    private static String extractTopLevelFieldName(String fieldName, int dotIndex) {
-        return fieldName.substring(0, dotIndex);
-    }
-
-    private static Object invokeMethod(String methodName, Object entity) {
-        Object methodInvocationResult;
-        try {
-            Method method = entity.getClass().getMethod(methodName);
-            methodInvocationResult = method.invoke(entity);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-        return methodInvocationResult;
-    }
-
-    private static String getterMethodName(String fieldName) {
-        return "get" + fieldName.substring(0, 1).toUpperCase() + fieldName.substring(1);
     }
 }
