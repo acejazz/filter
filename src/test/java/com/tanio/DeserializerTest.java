1d9ecfdc935366eedb71e8149253b066cf940a7e
taniomessina <taniomessina@gmail.com>
2023-03-21 21:35:05 +0000

    Add deserialization for CompoundCondition

--- src/test/java/com/tanio/DeserializerTest.java
+++ src/test/java/com/tanio/DeserializerTest.java
@@ -3,15 +3,17 @@ package com.tanio;
 import com.fasterxml.jackson.core.JacksonException;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
 import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.databind.node.JsonNodeType;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -37,23 +39,27 @@ class DeserializerTest {
     @Test
     void deserializeSimpleConditionArray() throws JsonProcessingException {
         String json = """
-                [
-                    {
-                        "fieldName":"anyFieldName0",
-                        "operator":"EQUAL",
-                        "value":"anyValue0"
-                    },
-                    {
-                        "fieldName":"anyFieldName1",
-                        "operator":"NOT_EQUAL",
-                        "value":"anyValue1"
-                    }
-                ]
+                {
+                    "and":
+                        [
+                            {
+                                "fieldName":"anyFieldName0",
+                                "operator":"EQUAL",
+                                "value":"anyValue0"
+                            },
+                            {
+                                "fieldName":"anyFieldName1",
+                                "operator":"NOT_EQUAL",
+                                "value":"anyValue1"
+                            }
+                        ]
+                }
                 """;
 
         Deserializer deserializer = new Deserializer();
-        Set<SimpleCondition> result = deserializer.deserializeList(json);
-        assertThat(result).containsExactlyInAnyOrder(
+        CompoundCondition result = deserializer.deserializeCompoundCondition(json);
+        assertThat(result.getOperator()).isEqualTo(CompoundCondition.BooleanOperator.AND);
+        assertThat(result.getConditions()).containsExactlyInAnyOrder(
                 new SimpleCondition("anyFieldName0", SimpleCondition.Operator.EQUAL, "anyValue0"),
                 new SimpleCondition("anyFieldName1", SimpleCondition.Operator.NOT_EQUAL, "anyValue1"));
     }
@@ -68,12 +74,27 @@ class Deserializer {
         return objectMapper.readValue(json, SimpleCondition.class);
     }
 
-    Set<SimpleCondition> deserializeList(String json) throws JsonProcessingException {
+    CompoundCondition deserializeCompoundCondition(String json) throws JsonProcessingException {
         ObjectMapper objectMapper = new ObjectMapper();
-        SimpleModule module = new SimpleModule();
-        module.addDeserializer(SimpleCondition.class, new SimpleConditionDeserializer());
-        objectMapper.registerModule(module);
-        return objectMapper.readValue(json, new TypeReference<>() { });
+        OperatorMapper operatorMapper = new OperatorMapper();
+
+        JsonNode jsonNode = objectMapper.readTree(json);
+        JsonNode and = jsonNode.get("and");
+        Set<Condition> simpleConditions = new HashSet<>();
+        if (and.getNodeType().equals(JsonNodeType.ARRAY)) {
+            Iterator<JsonNode> elements = and.elements();
+            while (elements.hasNext()) {
+                JsonNode next = elements.next();
+                String fieldName = next.get("fieldName").asText();
+                String operator = next.get("operator").asText();
+                String value = next.get("value").asText();
+                SimpleCondition simpleCondition =
+                        new SimpleCondition(fieldName, operatorMapper.map(operator), value);
+                simpleConditions.add(simpleCondition);
+            }
+        }
+
+        return new CompoundCondition(CompoundCondition.BooleanOperator.AND, simpleConditions);
     }
 }
 
