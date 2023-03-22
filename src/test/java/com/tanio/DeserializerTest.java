658de880f11c36e38d040305b5819dcaf9b88bc4
taniomessina <taniomessina@gmail.com>
2023-03-22 21:35:52 +0000

    Refactor

--- src/test/java/com/tanio/DeserializerTest.java
+++ src/test/java/com/tanio/DeserializerTest.java
@@ -13,6 +13,8 @@ import java.util.Set;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class DeserializerTest {
+    Deserializer sut = new Deserializer();
+
     @Test
     void deserializeAndCompoundCondition() throws JsonProcessingException {
         String json = """
@@ -33,8 +35,7 @@ class DeserializerTest {
                 }
                 """;
 
-        Deserializer deserializer = new Deserializer();
-        CompoundCondition result = deserializer.deserializeCompoundCondition(json);
+        CompoundCondition result = (CompoundCondition) sut.deserialize(json);
         assertThat(result.getOperator()).isEqualTo(CompoundCondition.BooleanOperator.AND);
         assertThat(result.getConditions()).containsExactlyInAnyOrder(
                 new SimpleCondition("anyFieldName0", SimpleCondition.Operator.EQUAL, "anyValue0"),
@@ -51,8 +52,7 @@ class DeserializerTest {
                 }
                 """;
 
-        Deserializer deserializer = new Deserializer();
-        SimpleCondition result = deserializer.deserializeSimpleCondition(json);
+        SimpleCondition result = (SimpleCondition) sut.deserialize(json);
         assertThat(result.getFieldName()).isEqualTo("anyFieldName");
         assertThat(result.getOperator()).isEqualTo(SimpleCondition.Operator.EQUAL);
         assertThat(result.getValue()).isEqualTo("anyValue");
@@ -60,34 +60,36 @@ class DeserializerTest {
 }
 
 class Deserializer {
-    CompoundCondition deserializeCompoundCondition(String json) throws JsonProcessingException {
-        ObjectMapper objectMapper = new ObjectMapper();
-        OperatorMapper operatorMapper = new OperatorMapper();
+    ObjectMapper objectMapper = new ObjectMapper();
+    OperatorMapper operatorMapper = new OperatorMapper();
 
+    Condition deserialize(String json) throws JsonProcessingException {
         JsonNode jsonNode = objectMapper.readTree(json);
+        if (jsonNode.has("and")) {
+            return deserializeCompoundCondition(jsonNode);
+        }
+        return deserializeSimpleCondition(jsonNode);
+    }
+
+    private CompoundCondition deserializeCompoundCondition(JsonNode jsonNode) throws JsonProcessingException {
         JsonNode and = jsonNode.get("and");
         Set<Condition> simpleConditions = new HashSet<>();
         if (and.getNodeType().equals(JsonNodeType.ARRAY)) {
             Iterator<JsonNode> elements = and.elements();
             while (elements.hasNext()) {
                 JsonNode next = elements.next();
-                String fieldName = next.get("fieldName").asText();
-                String operator = next.get("operator").asText();
-                String value = next.get("value").asText();
-                SimpleCondition simpleCondition =
-                        new SimpleCondition(fieldName, operatorMapper.map(operator), value);
-                simpleConditions.add(simpleCondition);
+                simpleConditions.add(toSimpleCondition(next));
             }
         }
 
         return new CompoundCondition(CompoundCondition.BooleanOperator.AND, simpleConditions);
     }
 
-    public SimpleCondition deserializeSimpleCondition(String json) throws JsonProcessingException {
-        ObjectMapper objectMapper = new ObjectMapper();
-        OperatorMapper operatorMapper = new OperatorMapper();
+    private SimpleCondition deserializeSimpleCondition(JsonNode jsonNode) {
+        return toSimpleCondition(jsonNode);
+    }
 
-        JsonNode jsonNode = objectMapper.readTree(json);
+    private SimpleCondition toSimpleCondition(JsonNode jsonNode) {
         String fieldName = jsonNode.get("fieldName").asText();
         String operator = jsonNode.get("operator").asText();
         String value = jsonNode.get("value").asText();
