53070da602d864925ce1c5bd2e046b2ac82d7f8c
taniomessina <taniomessina@gmail.com>
2023-03-22 21:33:39 +0000

    Add deserialization for SimpleCondition

--- src/test/java/com/tanio/DeserializerTest.java
+++ src/test/java/com/tanio/DeserializerTest.java
@@ -1,17 +1,11 @@
 package com.tanio;
 
-import com.fasterxml.jackson.core.JacksonException;
-import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
-import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.fasterxml.jackson.databind.node.JsonNodeType;
 import org.junit.jupiter.api.Test;
 
-import java.io.IOException;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
@@ -20,24 +14,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class DeserializerTest {
     @Test
-    void deserializeSimpleCondition() throws JsonProcessingException {
-        String json = """
-                {
-                    "fieldName":"anyFieldName",
-                    "operator":"EQUAL",
-                    "value":"anyValue"
-                }
-                """;
-
-        Deserializer deserializer = new Deserializer();
-        SimpleCondition result = deserializer.deserialize(json);
-        assertThat(result.getFieldName()).isEqualTo("anyFieldName");
-        assertThat(result.getOperator()).isEqualTo(SimpleCondition.Operator.EQUAL);
-        assertThat(result.getValue()).isEqualTo("anyValue");
-    }
-
-    @Test
-    void deserializeSimpleConditionArray() throws JsonProcessingException {
+    void deserializeAndCompoundCondition() throws JsonProcessingException {
         String json = """
                 {
                     "and":
@@ -63,17 +40,26 @@ class DeserializerTest {
                 new SimpleCondition("anyFieldName0", SimpleCondition.Operator.EQUAL, "anyValue0"),
                 new SimpleCondition("anyFieldName1", SimpleCondition.Operator.NOT_EQUAL, "anyValue1"));
     }
+
+    @Test
+    void deserializeSimpleCondition() throws JsonProcessingException {
+        String json = """
+                {
+                    "fieldName":"anyFieldName",
+                    "operator":"EQUAL",
+                    "value":"anyValue"
+                }
+                """;
+
+        Deserializer deserializer = new Deserializer();
+        SimpleCondition result = deserializer.deserializeSimpleCondition(json);
+        assertThat(result.getFieldName()).isEqualTo("anyFieldName");
+        assertThat(result.getOperator()).isEqualTo(SimpleCondition.Operator.EQUAL);
+        assertThat(result.getValue()).isEqualTo("anyValue");
+    }
 }
 
 class Deserializer {
-    SimpleCondition deserialize(String json) throws JsonProcessingException {
-        ObjectMapper objectMapper = new ObjectMapper();
-        SimpleModule module = new SimpleModule();
-        module.addDeserializer(SimpleCondition.class, new SimpleConditionDeserializer());
-        objectMapper.registerModule(module);
-        return objectMapper.readValue(json, SimpleCondition.class);
-    }
-
     CompoundCondition deserializeCompoundCondition(String json) throws JsonProcessingException {
         ObjectMapper objectMapper = new ObjectMapper();
         OperatorMapper operatorMapper = new OperatorMapper();
@@ -96,23 +82,15 @@ class Deserializer {
 
         return new CompoundCondition(CompoundCondition.BooleanOperator.AND, simpleConditions);
     }
-}
 
-class SimpleConditionDeserializer extends StdDeserializer<SimpleCondition> {
+    public SimpleCondition deserializeSimpleCondition(String json) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        OperatorMapper operatorMapper = new OperatorMapper();
 
-    OperatorMapper operatorMapper = new OperatorMapper();
-
-    SimpleConditionDeserializer() {
-        super((Class<?>) null);
-    }
-
-    @Override
-    public SimpleCondition deserialize(JsonParser jsonParser,
-                                       DeserializationContext deserializationContext) throws IOException, JacksonException {
-        JsonNode node = jsonParser.getCodec().readTree(jsonParser);
-        String fieldName = node.get("fieldName").asText();
-        String operator = node.get("operator").asText();
-        String value = node.get("value").asText();
+        JsonNode jsonNode = objectMapper.readTree(json);
+        String fieldName = jsonNode.get("fieldName").asText();
+        String operator = jsonNode.get("operator").asText();
+        String value = jsonNode.get("value").asText();
         return new SimpleCondition(fieldName, operatorMapper.map(operator), value);
     }
 }
