e983eced4fb840121227fa74de11406f67cde2b3
taniomessina <taniomessina@gmail.com>
2023-03-29 22:02:49 +0200

    Refactor

--- src/main/java/com/tanio/FieldValueRetriever.java
+++ src/main/java/com/tanio/FieldValueRetriever.java
@@ -7,55 +7,73 @@ class FieldValueRetriever {
     private static final String SEPARATOR = ".";
 
     Object retrieveFieldValue(String fieldName, Object object) {
-        int separatorIndex = fieldName.indexOf(SEPARATOR);
-
-        if (separatorIndex == -1) {
-            boolean isFieldNameReferringToBoolean = fieldName.startsWith("is")
-                    && Character.isUpperCase(fieldName.charAt(2));
-            if (isFieldNameReferringToBoolean) {
-                try {
-                    Method method = object.getClass().getMethod(fieldName);
-                    boolean isReturnTypeBoolean = method.getReturnType().equals(boolean.class)
-                            || method.getReturnType().equals(Boolean.class);
-                    if (isReturnTypeBoolean) {
-                        return invokeMethod(fieldName, object);
-                    }
-                } catch (NoSuchMethodException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-
-            String methodName = transformToGetterMethodName(fieldName);
-            return invokeMethod(methodName, object);
+        if (isNestedFieldName(fieldName)) {
+            String topLevelFieldName = extractTopLevelFieldName(fieldName);
+            String getterMethodName = transformToGetterMethodName(topLevelFieldName);
+            Object topLevelFieldValue = invokeMethod(getterMethodName, object);
+            String remainingLevelFieldNames = extractRemainingLevelFieldNames(fieldName);
+            return retrieveFieldValue(remainingLevelFieldNames, topLevelFieldValue);
         }
 
-        String topLevelFieldName = extractTopLevelFieldName(fieldName, separatorIndex);
-        String methodName = transformToGetterMethodName(topLevelFieldName);
-        Object topLevelFieldValue = invokeMethod(methodName, object);
-        String remainingLevelFieldNames = extractRemainingLevelFieldNames(fieldName, separatorIndex);
-        return retrieveFieldValue(remainingLevelFieldNames, topLevelFieldValue);
+        if (isFieldNameReferringToBoolean(fieldName)) {
+            Method method = getMethod(fieldName, object);
+            if (isReturnTypeBoolean(method)) {
+                // Does not use the transformation to getter name
+                return invokeMethod(fieldName, object);
+            }
+        }
+
+        String methodName = transformToGetterMethodName(fieldName);
+        return invokeMethod(methodName, object);
+    }
+
+    boolean isNestedFieldName(String fieldName) {
+        int separatorIndex = fieldName.indexOf(SEPARATOR);
+        return separatorIndex != -1;
+    }
+
+    private static boolean isReturnTypeBoolean(Method method) {
+        return method.getReturnType().equals(boolean.class)
+                || method.getReturnType().equals(Boolean.class);
+    }
+
+    private static boolean isFieldNameReferringToBoolean(String fieldName) {
+        return fieldName.startsWith("is")
+                && Character.isUpperCase(fieldName.charAt(2));
     }
 
     private static String transformToGetterMethodName(String fieldName) {
         return "get" + fieldName.substring(0, 1).toUpperCase() + fieldName.substring(1);
     }
 
-    private static String extractTopLevelFieldName(String fieldName, int dotIndex) {
-        return fieldName.substring(0, dotIndex);
+    private static String extractTopLevelFieldName(String fieldName) {
+        int separatorIndex = fieldName.indexOf(SEPARATOR);
+        return fieldName.substring(0, separatorIndex);
     }
 
     private static Object invokeMethod(String methodName, Object object) {
-        Object methodInvocationResult;
+        Method method = getMethod(methodName, object);
+        return invokeMethod(method, object);
+    }
+
+    private static Method getMethod(String methodName, Object object) {
         try {
-            Method method = object.getClass().getMethod(methodName);
-            methodInvocationResult = method.invoke(object);
+            return object.getClass().getMethod(methodName);
+        } catch (NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static Object invokeMethod(Method method, Object object) {
+        try {
+            return method.invoke(object);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
-        return methodInvocationResult;
     }
 
-    private static String extractRemainingLevelFieldNames(String fieldName, int dotIndex) {
-        return fieldName.substring(dotIndex + 1);
+    private static String extractRemainingLevelFieldNames(String fieldName) {
+        int separatorIndex = fieldName.indexOf(SEPARATOR);
+        return fieldName.substring(separatorIndex + 1);
     }
 }
